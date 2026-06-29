2c6fe8fe6b6f15451e468962b9f69dee94ec4a96
tha <tharit.puts@gmail.com>
2026-06-29 17:59:04 +0700

    feat: implement dynamic greeting message with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => setGreeting("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => setGreeting("Hi!")}>Hi!</button>
+        <button onClick={() => setGreeting("你好!")}>你好!</button>
       </div>
     </div>
   );
